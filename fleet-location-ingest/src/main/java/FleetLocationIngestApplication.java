7edbc064baad728c77a2d9d96cf82bf1ff9fefb3
smartlin5228 <smartlin1@gmail.com>
2016-12-02 02:11:19 -0500

    Finished fleet location ingest

---
+++ fleet-location-ingest/src/main/java/FleetLocationIngestApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetLocationIngestApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FleetLocationIngestApplication.class, args);
+    }
+}
---
+++ fleet-location-ingest/src/main/java/VehiclePositionsSource.java
@@ -0,0 +1,21 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableBinding(Source.class)
+@RestController
+public class VehiclePositionsSource {
+    @Autowired
+    private MessageChannel output;
+
+    @RequestMapping(path = "/api/locations", method = RequestMethod.POST)
+    public void locations(@RequestBody String positionInfo) {
+        this.output.send(MessageBuilder.withPayload(positionInfo).build());
+    }
+}
